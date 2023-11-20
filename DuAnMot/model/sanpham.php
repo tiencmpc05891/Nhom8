0e3a26258533858068bb5d44a42ec06e261b4283
tiencmpc05891 <145202677+tiencmpc05891@users.noreply.github.com>
2023-11-20 21:20:53 +0700

    update model sanpham
    đổi các function, update thêm function new, nổi bật

--- DuAnMot/model/sanpham.php
+++ DuAnMot/model/sanpham.php
@@ -70,98 +70,15 @@ function update_sanpham($id, $iddm, $tensp, $giasp, $mota, $thongtin, $hinh)
     pdo_execute($sql);
 }
 
-
-// function get_dssp_new($id)
-// {
-//     $sql = "SELECT * FROM sanpham ORDER BY id DESC limit " . $id;
-//     return pdo_query($sql);
-// }
-// function get_dssp_best($limi)
-// {
-//     $sql = "SELECT * FROM sanpham WHERE bestseller=1 ORDER BY id DESC limit " . $limi;
-//     return pdo_query($sql);
-// }
-// function get_dssp_view($limi)
-// {
-//     $sql = "SELECT * FROM sanpham ORDER BY view DESC limit " . $limi;
-//     return pdo_query($sql);
-// }
-function dsspall($spnew)
-{
-    $html_dssp = '';
-    include "global.php";
-
-    // Giới hạn hiển thị tối đa 8 sản phẩm
-    $count = 0;
-
-    foreach ($spnew as $sp) {
-        if ($count >= 8) {
-            break; // Đã hiển thị đủ 8 sản phẩm, thoát khỏi vòng lặp
-        }
-
-        extract($sp);
-        $linksp = "index.php?act=sanphamct&idsp=" . $id;
-        // $hinh = $img_path . $img;
-        $img = $img_path . $img;
-
-        $html_dssp .= '<div class="box25 mr15">
-            <div  
-                class="row img">
-                <a href="' . $linksp . '">
-                    <img src="' . $img . '" alt="' . $name . '">
-                </a>
-            </div>
-            <p><strong style="color:red;">' . number_format($price) . '<sup>đ</sup></strong></p>
-            <a  href="' . $linksp . '">' . $name . '</a>
-            <form action="index.php?act=addtocart" method="post">
-                <input type="hidden" name="id" value="' . $id . '">
-                <input type="hidden" name="name" value="' . $name . '">
-                <input type="hidden" name="img" value="' . $img . '">
-                <input type="hidden" name="price" value="' . $price . '"> <br>
-                <input type="submit" name="addtocart" value="Thêm vào giỏ hàng">
-                </br> </br>
-            </form>
-   
-            <br>  <br>
-        </div>';
-
-        $count++;
-    }
-
-    return $html_dssp;
+function loadall_sanpham_noibat() {
+    $sql = "SELECT * FROM sanpham WHERE iddm = 9 ORDER BY id DESC LIMIT 0,8";
+    $listsanpham = pdo_query($sql);
+    return $listsanpham;
 }
-
-
-function showdsyt($dstopsp)
-{
-
-    $html_dssp = '';
-    include "global.php";
-    foreach ($dstopsp as $sp) {
-        extract($sp);
-        $linksp = "index.php?act=sanphamct&idsp=" . $id;
-        $img = $img_path . $img;
-        $html_dssp .= '<div class="box25 mr15">
-            <div class="row img">
-                <a href="' . $linksp . '">
-                    <img src="' . $img . '" alt="' . $name . '">
-                </a>
-            </div>
-            <p><strong style="color:red;">' . number_format($price) . '<sup>đ</sup></strong></p>
-            <a style="text-decoration: none;" href="' . $linksp . '">' . $name . '</a>
-            <form action="index.php?act=addtocart" method="post">
-                <input type="hidden" name="id" value="' . $id . '">
-                <input type="hidden" name="name" value="' . $name . '">
-                <input type="hidden" name="img" value="' . $img . '">
-                <input type="hidden" name="price" value="' . $price . '">
-         
-                <input type="submit" name="addtocart" value="Thêm vào giỏ hàng">
-                </br> </br>
-                </form>
-              
-                <br>  <br>
-        </div>';
-    }
-
-    return $html_dssp;
+function loadall_sanpham_hot() {
+    $sql = "SELECT * FROM sanpham WHERE iddm = 8 ORDER BY id DESC LIMIT 0,8";
+    $listsanpham = pdo_query($sql);
+    return $listsanpham;
 }
+// Sửa đổi hàm Loadall_sanpham để không lấy sản phẩm thuộc danh mục nổi bật
+?>
