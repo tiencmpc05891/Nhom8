4356d20b06b6f56dcb3d3102dd9edb15e5312b8f
tiencmpc05891 <145202677+tiencmpc05891@users.noreply.github.com>
2023-11-26 22:06:21 +0700

    update model cart lần 2
    thêm các function mới của cart

--- DuAnMot/model/cart.php
+++ DuAnMot/model/cart.php
@@ -1,62 +1,171 @@
 <?php
 
-function viewcart($del)
+function showcart($del, $isCheckout = false)
 {
-
     global $img_path;
     $tong = 0;
     $i = 0;
     if ($del == 1) {
-        $xoasp_th = '<th>Thao tác</th>
-        ';
+        $xoasp_th = '<th>Thao tác</th>';
         $xoasp_td2 = '<td></td>';
     } else {
         $xoasp_th = '';
         $xoasp_td2 = '';
     }
-    echo '<tr>
-    <th>Hình</th>
-    <th>Sản Phẩm</th>
-    <th>Đơn giá</th>
-    <th>Số lượng</th>
-    <th>Thành tiền</th>
-    ' . $xoasp_th . '
-    </tr>';
 
-    foreach ($_SESSION['mycart'] as $cart) {
-        // $img = $img_path . $cart[2];
-        $img = $cart[2];
+    echo '<div class="container-fluid">
+            <div class="row px-xl-5">';
 
+    if ($isCheckout) {
+        echo '<div class="col-lg-12">
+                <table class="table table-light table-borderless text-center mb-0">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>Sản Phẩm</th>
+                            <th>Đơn giá</th>
+                        </tr>
+                    </thead>
+                    <tbody class="align-middle">';
 
-        $price = floatval($cart[3]);
-        $soluong = intval($cart[4]); #floatval() hoặc intval() để chuyển chuỗi thành số nguyên hoặc số thực.
-        $thanhtien = $price * $soluong;
+        foreach ($_SESSION['mycart'] as $cart) {
+            $price = floatval($cart[3]);
+            $thanhtien =$price* $cart[4];
+            $tong += $thanhtien;
 
-        $tong += $thanhtien;
-        if ($del == 1) {
-            $xoasp_td = '<td><a href="index.php?act=delcart&idcart=' . $i . '"><input type="button" value="Xóa"></a></td>';
-        } else {
-
-            $xoasp_td = '';
+            echo '<tr>';
+            echo '    <td class="align-middle">' . $cart[1] . '</td>';
+            echo '    <td class="align-middle">' . $cart[3] . '<sup>đ</sup></td>';
+            echo '</tr>';
         }
 
-        echo '<tr>
-        <td><img src="' . $img . '" alt="" height="80px"></td>
-        <td>' . $cart[1] . '</td>
-        <td>' . $cart[3] . '<sup>đ</sup></td>
-        <td>' . $cart[4] . '</td>
-        <td>' . $thanhtien . '<sup>đ</sup></td>
-        ' . $xoasp_td . '
-        </tr>';
+        echo '</tbody>';
 
-        $i += 1;
+        echo '    </table>';
+        echo '    </div>
+        <div class="pt-2">
+            <div class="d-flex justify-content-between mt-2">
+                <h5>Tổng cộng: ' . number_format($tong, 0, '.', '.') . '<sup>đ</sup></h5>
+                <h5></h5>
+            </div>';
+    } else {
+        echo '<div class="col-lg-8 table-responsive mb-5">
+                <table class="table table-light table-borderless table-hover text-center mb-0">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Hình</th>
+                            <th>Sản Phẩm</th>
+                            <th>Đơn giá</th>
+                            <th>Số lượng</th>
+                            <th>Thành tiền</th>
+                            ' . $xoasp_th . '
+                        </tr>
+                    </thead>
+                    <tbody class="align-middle">';
+
+        foreach ($_SESSION['mycart'] as $cart) {
+            $price = floatval($cart[3]);
+            $soluong = intval($cart[4]);
+            $thanhtien = $price * $soluong;
+            $tong += $thanhtien;
+
+            if ($del == 1) {
+                $xoasp_td = '<td><a href="index.php?act=delcart&idcart=' . $i . '"><input type="button" value="Xóa"></a></td>';
+            } else {
+                $xoasp_td = '';
+            }
+
+            echo '<tr>';
+            echo '    <td class="align-middle"><img src="' . $cart[2] . '" alt="" height="80px"></td>';
+            echo '    <td class="align-middle">' . $cart[1] . '</td>';
+            echo '    <td class="align-middle">' . $cart[3] . '<sup>đ</sup></td>';
+            echo '    <td class="align-middle">
+                        <button class="btn btn-primary btn-minus" onclick="updateQuantity(' . $i . ', -1)">-</button>
+                        <span id="quantity-' . $i . '">' . $cart[4] . '</span>
+                        <button class="btn btn-primary btn-plus" onclick="updateQuantity(' . $i . ', 1)">+</button>
+                    </td>';
+            echo '    <td class="align-middle">' . number_format($thanhtien, 0, '.', '.') . '<sup>đ</sup></td>';
+            echo '    ' . $xoasp_td;
+            echo '</tr>';
+
+            $i += 1;
+        }
+
+        echo '</tbody>';
+        
+        echo '            <tfoot>
+                            <tr>
+                                <td colspan="4">Tổng đơn hàng</td>
+                                <td>' . number_format($tong, 0, '.', '.') . '<sup>đ</sup></td>
+                                ' . $xoasp_td2 . '
+                            </tr>
+                            
+                        </tfoot>';
+
+        echo '        </table>
+
+        <a href="index.php?act=shop"><input class="btn btn-success" type="button" value="Tiếp tục đặt hàng"></a>     <a href="index.php?act=delcart"><input class="btn btn-danger" type="button" value="Xóa tất cả giỏ hàng"></a>
+       </div>';
+            
     }
-    echo ' <tr>
-                <td colspan="4">Tổng đơn hàng</td>
-                <td>' . $tong . '<sup>đ</sup></td>
-             ' . $xoasp_td2 . '
-        </tr>';
+
+    echo '<div class="col-lg-4">';
+    
+    if (!$isCheckout) {
+        echo '<form class="mb-3" action="">
+                <div class="input-group">
+                    <input type="text" class="form-control border-0 p-4" placeholder="Coupon Code">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary">Áp dụng mã giảm giá</button>
+                    </div>
+                </div>
+            </form>';
+    }
+    echo '<div class="bg-light p-3 mb-3">
+            <div class="border-bottom pb-2">
+                <div class="d-flex justify-content-between mb-3">
+                </div>';
+    if (!$isCheckout) {
+        echo '        <div class="d-flex justify-content-between">
+            
+                                <h6 class="font-weight-medium">Tổng đơn hàng : </h6>
+                                <h6 class="font-weight-medium">' . number_format($tong, 0, '.', '.') . '<sup>đ</sup></h6>
+            
+                            </div>';
+    }
+    if (!$isCheckout) {
+        echo '        <div class="d-flex justify-content-between">
+
+                    <h6 class="font-weight-medium">Phí ship</h6>
+                    <h6 class="font-weight-medium">0đ</h6>
+
+                </div>';
+    }
+
+
+    if (!$isCheckout) {
+        echo '    </div>
+        <div class="pt-2">
+            <div class="d-flex justify-content-between mt-2">
+                <h5>Thành tiền:</h5>
+                <h5>' . number_format($tong, 0, '.', '.') . '<sup>đ</sup></h5>
+            </div>';
+    }
+    if (!$isCheckout) {
+        echo '    <a href="index.php?act=checkout">
+                    <button class="btn btn-block btn-primary font-weight-bold my-3 py-3">Tiếp tục thanh toán</button>
+
+                </a>';
+    }
+    echo '        </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>';
 }
+
+
+
 
 function bill_chi_tiet($listbill)
 {
@@ -73,7 +182,7 @@ function bill_chi_tiet($listbill)
     </tr>';
 
     foreach ($listbill as $cart) {
-        $img =$cart['img']; // Sử dụng $cart['img'] để truy cập URL hình ảnh
+        $img = $cart['img']; // Sử dụng $cart['img'] để truy cập URL hình ảnh
         $tong += $cart['thanhtien'];
 
         echo '
@@ -102,18 +211,18 @@ function tongdonhang()
     }
     return $tong;
 }
-
+function insert_cart($iduser, $idpro, $img, $name, $price, $soluong, $thanhtien, $idbill)
+{
+    $sql = "insert into  cart(iduser,idpro,img,name,price,soluong,thanhtien,idbill) values('$iduser','$idpro','$img','$name','$price','$soluong','$thanhtien','$idbill')";
+    return pdo_execute($sql);
+}
 function insert_bill($iduser, $name, $email, $address, $tel, $pttt, $ngaydathang, $tongdonhang)
 {
     $sql = "insert into  bill(iduser,bill_name,bill_email,bill_address,bill_tel,bill_pttt,ngaydathang,total) values('$iduser','$name','$email','$address','$tel','$pttt','$ngaydathang','$tongdonhang')";
     return pdo_execute_return_lastInsertId($sql);
 }
 
-function insert_cart($iduser, $idpro, $img, $name, $price, $soluong, $thanhtien, $idbill)
-{
-    $sql = "insert into  cart(iduser,idpro,img,name,price,soluong,thanhtien,idbill) values('$iduser','$idpro','$img','$name','$price','$soluong','$thanhtien','$idbill')";
-    return pdo_execute($sql);
-}
+
 
 function loadone_bill($id)
 {
