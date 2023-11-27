5c9e7ed62efa7f894450815f3cca74d65b9e8ade
MHuy12 <145202827+MHuy12@users.noreply.github.com>
2023-11-28 02:33:45 +0700

    chức năng quản lí bài viết

---
+++ DuAnMot/admin/post/add.php
@@ -0,0 +1,120 @@
+<?php
+            if (isset($thongbao) && ($thongbao != ""))
+                echo $thongbao;
+
+
+            ?>
+        </form>
+    </div>
+</div>
+</div> 
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Thêm Mới Bài Viết</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active">Bài viết</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <form action="index.php?act=addbv" method="post" enctype="multipart/form-data">
+                                <div class="modal-body">
+                                    
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control" name="tieude" placeholder="Tiêu đề">
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control" name="noidung" placeholder="Nội dung">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="topic-name" class="col-form-label">Hình ảnh</label>
+                                        <input type="file" name="hinh" class="col-form-label">
+                                    </div>
+
+                                    
+                                </div>
+                                <div class="modal-footer justify-content-between">
+                                    <a href="index.php?act=listbv"> <input type="button" class="btn btn-primary" value="Danh sách"></input></a>
+
+                                    <input type="submit" name="themmoi" class="btn btn-success" value="Thêm mới"></input></a>
+
+                                </div>
+                                <?php
+                                if (isset($thongbao) && ($thongbao != ""))
+                                    echo $thongbao;
+
+
+                                ?>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+
+<div class="modal fade" id="modal-default">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Thêm bài viết</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="index.php?act=addsp" method="POST">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <input type="text" class="form-control" name="tieude" placeholder="Tiêu đề">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" name="noidung" placeholder="Nội dung">
+                    </div>
+                    <div class="mb-3">
+                        <label for="topic-name" class="col-form-label">Hình ảnh</label>
+                        <input type="file" name="img" class="col-form-label" id="">
+                    </div>
+                    
+                   
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <a href="index.php?act=listbv"> <input type="button" class="btn btn-primary" value="Danh sách"></input></a>
+
+                    <input type="submit" name="themmoi" class="btn btn-success" value="Thêm mới"></input></a>
+
+                </div>
+            </form>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
---
+++ DuAnMot/admin/post/list.php
@@ -0,0 +1,96 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Bài viết</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active">Bài viết</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="d-flex justify-content-end">
+                <a href="index.php?act=addbv"> <button type="button" class="btn btn-primary mb-2">
+                        Thêm bài viết mới
+                    </button></a>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <!-- <div class="card-header">
+                            <h3 class="card-title ">Danh sách chủ đề</h3>
+                        </div> -->
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Tiêu đề</th>
+                                        <th scope="col">Nội dung</th>
+                                        <th scope="col">Ảnh</th>
+                                    </tr>
+                                </thead>
+                                <?php
+                                foreach ($listbaiviet as $baiviet) {
+                                    extract($baiviet);
+                                    $suabv = "index.php?act=suabv&id=" . $id;
+                                    $xoabv = "index.php?act=xoabv&id=" . $id;
+                                    $hinhpath = "../upload/" . $img;
+                                    if (is_file($hinhpath)) {
+                                        $hinh = "<img src='" . $hinhpath . "' height='80' >";
+                                    } else {
+                                        $hinh = "không có hình";
+                                    }
+                                    echo '<tr>
+                <td>' . $id . '</td>
+                <td>' . $tieude . '</td>
+                <td>' . $noidung . '</td>
+                <td>' . $hinh . '</td>
+                <td><a href="' . $suabv . '"><input class="btn btn-warning" type="button"  value="Sửa"></a> <a href="' . $xoabv . '"><input type="button" class="btn btn-danger" value="Xóa"></a></td>
+              </tr>';
+                                }
+                                ?>
+                            </table>
+
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<div class="modal fade" id="modal-default">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Thêm bài viết</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
