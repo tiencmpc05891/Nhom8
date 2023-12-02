4fde7be5d5ba6de1e519e29b2749e9ff47a0992c
MHuy12 <145202827+MHuy12@users.noreply.github.com>
2023-12-03 01:40:36 +0700

    fix binh luan

--- DuAnMot/model/binhluan.php
+++ DuAnMot/model/binhluan.php
@@ -20,8 +20,43 @@ function delete_binhluan($id)
     $sql = "delete from binhluan where id=" . $id;
     pdo_execute($sql);
 }
+function delete_comment($id)
+{
+    // Thực hiện xóa bình luận từ cơ sở dữ liệu dựa trên ID
+    $sql = "DELETE FROM binhluan WHERE id = :id";
+    $params = array(':id' => $id);
+    pdo_execute($sql, $params);
+}
+
+function get_user_by_id($id)
+{
+    $sql = "SELECT * FROM taikhoan WHERE id = :id";
+    $params = array(':id' => $id);
+
+    try {
+        // Thêm tham số $params vào hàm pdo_execute
+        $user = pdo_query_one($sql, $params);
+
+        return $user;
+    } catch (PDOException $e) {
+        echo "Lỗi: " . $e->getMessage();
+        throw $e;
+    }
+}
 
 
+function update_comment($commentId, $editedMessage)
+{
+    // Thực hiện cập nhật bình luận trong cơ sở dữ liệu
+    $sql = "UPDATE binhluan SET noidung = :editedMessage WHERE id = :commentId";
+    $params = array(':editedMessage' => $editedMessage, ':commentId' => $commentId);
 
+    try {
+        pdo_execute($sql, $params);
+    } catch (PDOException $e) {
+        echo "Lỗi: " . $e->getMessage();
+        throw $e;
+    }
+}
 
 ?>
